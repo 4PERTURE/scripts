275de8af9506199fb4539f40a417cfdf0526f3a2
4PERTURE <slicey2001@gmail.com>
2018-10-19 20:25:43 +0000

    add cloning script

---
+++ trees.sh
@@ -0,0 +1,7 @@
+rm -rf device/xiaomi
+rm -rf vendor/xiaomi
+rm -rf kernel/xiaomi
+git clone https://github.com/4PERTURE/android_device_xiaomi_vince -b 9.0-aosip device/xiaomi/vince
+git clone https://github.com/4PERTURE/android_device_xiaomi_msm8953-common -b 9.0-aosip device/xiaomi/msm8953-common
+git clone https://github.com/4PERTURE/android_vendor_xiaomi -b 9.0-aosip vendor/xiaomi
+git clone https://github.com/4PERTURE/android_kernel_xiaomi_msm8953-b 9.0-aosip kernel/xiaomi/msm8953
