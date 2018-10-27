35b43a7d7980b8882917c26a72c3affef151dc2d
4PERTURE <38117317+4PERTURE@users.noreply.github.com>
2018-10-27 01:31:27 +0100

    Update this according to new trees

--- trees.sh
+++ trees.sh
@@ -1,7 +1,7 @@
 rm -rf device/xiaomi
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
-git clone https://github.com/4PERTURE/android_device_xiaomi_vince -b 9.0-aosip device/xiaomi/vince
-git clone https://github.com/4PERTURE/android_device_xiaomi_msm8953-common -b 9.0-aosip device/xiaomi/msm8953-common
-git clone https://github.com/4PERTURE/android_vendor_xiaomi -b 9.0-aosip vendor/xiaomi
-git clone https://github.com/4PERTURE/android_kernel_xiaomi_msm8953 -b 9.0-aosip kernel/xiaomi/msm8953
+git clone https://github.com/4PERTURE/android_device_xiaomi_vince -b test device/xiaomi/vince
+git clone https://github.com/4PERTURE/android_device_xiaomi_msm8953-common -b test device/xiaomi/msm8953-common
+git clone https://github.com/4PERTURE/android_vendor_xiaomi -b arrow-9.x vendor/xiaomi
+git clone https://github.com/rama982/kernel_xiaomi_msm8953 -b android-9.0-inline kernel/xiaomi/msm8953
