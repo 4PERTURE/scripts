742ff6c7c8f585ad60509cfcc20d9e894a3bf02f
4PERTURE <38117317+4PERTURE@users.noreply.github.com>
2018-11-30 22:10:22 +0000

    Rebase updoot

--- trees.sh
+++ trees.sh
@@ -2,7 +2,7 @@ rm -rf device/xiaomi
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 rm -rf packages/resources/devicesettings
-git clone https://github.com/4PERTURE/android_device_xiaomi_vince -b pie-generic device/xiaomi/vince
+git clone https://github.com/4PERTURE/android_device_xiaomi_vince -b pie device/xiaomi/vince
 git clone https://github.com/4PERTURE/android_vendor_xiaomi -b pie vendor/xiaomi
 git clone https://github.com/rama982/kernel_xiaomi_msm8953 -b android-9.0-inline kernel/xiaomi/vince
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
