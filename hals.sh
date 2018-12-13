6a613e0d88825d7f2eeb3548029a84a3c59f712f
4PERTURE <38117317+4PERTURE@users.noreply.github.com>
2018-12-13 22:05:10 +0000

    Formatting can make all the difference in the world

--- hals.sh
+++ hals.sh
@@ -2,6 +2,5 @@ rm -rf hardware/qcom/audio-caf/msm8996
 rm -rf hardware/qcom/media-caf/msm8996
 rm -rf hardware/qcom/display-caf/msm8996
 git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-16.0-caf-8996 hardware/qcom/display-caf/msm8996
-git clone
-https://github.com/LineageOS/android_hardware_qcom_media -b lineage-16.0-caf-8996 hardware/qcom/media-caf/msm8996
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-16.0-caf-8996 hardware/qcom/media-caf/msm8996
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-16.0-caf-8996 hardware/qcom/audio-caf/msm8996
