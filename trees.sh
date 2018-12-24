45c80c9326389eb388207df205998888a7dc7b4f
4PERTURE <38117317+4PERTURE@users.noreply.github.com>
2018-12-24 17:07:40 +0000

    Update

--- trees.sh
+++ trees.sh
@@ -4,5 +4,5 @@ rm -rf kernel/xiaomi
 rm -rf packages/resources/devicesettings
 git clone https://github.com/4PERTURE/android_device_xiaomi_vince -b pie device/xiaomi/vince
 git clone https://github.com/4PERTURE/android_vendor_xiaomi -b pie vendor/xiaomi
-git clone https://github.com/rama982/kernel_xiaomi_msm8953 -b android-9.0-inline kernel/xiaomi/vince
+git clone https://github.com/4PERTURE/kernel_xiaomi_msm8953-1 -b pie kernel/xiaomi/msm8953
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
